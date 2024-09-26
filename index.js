1cc9e1b53c8202c2da39b78d40023c9dbd6abd02
Aman Sandhu <asandhu5656@gmail.com>
2024-09-26 18:32:25 -0400

    Added times and dates for Saudi GP and cleaned up some of the functions and classes to be easier to read and understand.

--- index.js
+++ index.js
@@ -5,7 +5,83 @@ fetch("https://ergast.com/api/f1/current.json")
   .then(data => {
     console.log(data);
 
-    function getPracticeLocalTimes(data) {
+    // Function that pulls Bahrain Practice and Qualifying session times from api and concats them to be formatted later on.
+    function getBahrainLocalTimes(data) {
+      const practices = [
+        "FirstPractice",
+        "SecondPractice",
+        "ThirdPractice",
+        "Qualifying",
+      ];
+
+      // Create a variable for each practice time
+      let firstPracBahrain,
+        secondPracBahrain,
+        thirdPracbahrain,
+        qualifyingBahrain;
+
+      practices.forEach((practice, index) => {
+        const practiceData = data.MRData.RaceTable.Races[0][practice];
+        if (!practiceData) {
+          throw new Error(`${practice} data not found.`);
+        }
+
+        const { time, date } = practiceData;
+        const localTime = `${date}T${time}`;
+
+        // Assign to the respective variable based on index
+        if (index === 0) firstPracBahrain = localTime;
+        if (index === 1) secondPracBahrain = localTime;
+        if (index === 2) thirdPracbahrain = localTime;
+        if (index === 3) qualifyingBahrain = localTime;
+      });
+
+      return {
+        firstPracBahrain,
+        secondPracBahrain,
+        thirdPracbahrain,
+        qualifyingBahrain,
+      };
+    }
+
+    const {
+      firstPracBahrain,
+      secondPracBahrain,
+      thirdPracbahrain,
+      qualifyingBahrain,
+    } = getBahrainLocalTimes(data);
+
+    // Function that pulls Bahrain Grand Prix times from api and concats them to be formatted later on.
+    function getBahrainRaceTimes(data) {
+      const raceTime = data.MRData.RaceTable.Races[0].time;
+      const raceDate = data.MRData.RaceTable.Races[0].date;
+
+      let raceLocal = `${raceDate}T${raceTime}`;
+
+      return raceLocal;
+    }
+    const bahrainRaceLocal = getBahrainRaceTimes(data);
+
+    class ConvertLocalTime {
+      constructor(time) {
+        this.time = time;
+        this.options = {
+          timeZone: "America/New_York",
+          timeZoneName: "short",
+          hour: "numeric",
+          minute: "numeric",
+        };
+        this.formatter = new Intl.DateTimeFormat([], this.options);
+      }
+
+      getLocalDate() {
+        const localDate = this.formatter.format(new Date(this.time));
+        return localDate;
+      }
+    }
+
+    // Function that pulls Saudi Practice and Qualifying session times from api and concats them to be formatted later on.
+    function getSaudiLocalTimes(data) {
       const practices = [
         "FirstPractice",
         "SecondPractice",
@@ -17,7 +93,7 @@ fetch("https://ergast.com/api/f1/current.json")
       let firstPracLocal, secondPracLocal, thirdPracLocal, qualifyingLocal;
 
       practices.forEach((practice, index) => {
-        const practiceData = data.MRData.RaceTable.Races[0][practice];
+        const practiceData = data.MRData.RaceTable.Races[1][practice];
         if (!practiceData) {
           throw new Error(`${practice} data not found.`);
         }
@@ -40,79 +116,21 @@ fetch("https://ergast.com/api/f1/current.json")
       };
     }
 
-    // Usage
     const { firstPracLocal, secondPracLocal, thirdPracLocal, qualifyingLocal } =
-      getPracticeLocalTimes(data);
-    console.log(firstPracLocal);
-    console.log(secondPracLocal);
-    console.log(thirdPracLocal);
-    console.log(qualifyingLocal);
+      getSaudiLocalTimes(data);
 
-    const firstPracTimeBahrain =
-      data.MRData.RaceTable.Races[0].FirstPractice.time;
-    const firstPracDateBahrain =
-      data.MRData.RaceTable.Races[0].FirstPractice.date;
+    // Function that pulls saudi Grand Prix times from api and concats them to be formatted later on.
+    function getSaudiRaceTimes(data) {
+      const raceTime = data.MRData.RaceTable.Races[1].time;
+      const raceDate = data.MRData.RaceTable.Races[1].date;
 
-    let firstPracBahrainLocal = firstPracDateBahrain.concat(
-      "T",
-      firstPracTimeBahrain
-    );
+      let raceLocal = `${raceDate}T${raceTime}`;
 
-    const secondPracTimeBahrain =
-      data.MRData.RaceTable.Races[0].SecondPractice.time;
-    const secondPracDateBahrain =
-      data.MRData.RaceTable.Races[0].SecondPractice.date;
-
-    let secondPracBahrainLocal = secondPracDateBahrain.concat(
-      "T",
-      secondPracTimeBahrain
-    );
-
-    const thirdPracTimeBahrain =
-      data.MRData.RaceTable.Races[0].ThirdPractice.time;
-    const thirdPracDateBahrain =
-      data.MRData.RaceTable.Races[0].ThirdPractice.date;
-
-    let thirdPracBahrainLocal = thirdPracDateBahrain.concat(
-      "T",
-      thirdPracTimeBahrain
-    );
-
-    const bahrainQualiTime = data.MRData.RaceTable.Races[0].Qualifying.time;
-    const bahrainQualiDate = data.MRData.RaceTable.Races[0].Qualifying.date;
-
-    let bahrainQualiLocal = bahrainQualiDate.concat("T", bahrainQualiTime);
-
-    const bahrainRaceTime = data.MRData.RaceTable.Races[0].time;
-    const bahrainRaceDate = data.MRData.RaceTable.Races[0].date;
-
-    let bahrainRaceLocal = bahrainRaceDate.concat("T", bahrainRaceTime);
-
-    class FP1TimeBahrain {
-      constructor(time) {
-        this.time = time;
-        this.options = {
-          timeZone: "America/New_York",
-          timeZoneName: "short",
-          hour: "numeric",
-          minute: "numeric",
-        };
-        this.formatter = new Intl.DateTimeFormat([], this.options);
-      }
-
-      getLocalDate() {
-        const localDate = this.formatter.format(new Date(this.time));
-        return localDate;
-      }
+      return raceLocal;
     }
+    const saudiRaceLocal = getSaudiRaceTimes(data);
 
-    const bahrainFp1Time = new FP1TimeBahrain(firstPracLocal);
-    const bahrainFp2Time = new FP1TimeBahrain(secondPracLocal);
-    const bahrainFp3Time = new FP1TimeBahrain(thirdPracLocal);
-    // const bahrainQualiTimeLocal = new FP1TimeBahrain(bahrainQualiLocal);
-    // const bahrainGpTimeLocal = new FP1TimeBahrain(bahrainRaceLocal);
-
-    class FP1DateBahrain {
+    class ConvertLocalDate {
       constructor(date) {
         this.date = date;
         this.options = {
@@ -127,16 +145,30 @@ fetch("https://ergast.com/api/f1/current.json")
         return localTimeBahrain;
       }
     }
+    const bahrainFp1Time = new ConvertLocalTime(firstPracBahrain);
+    const bahrainFp2Time = new ConvertLocalTime(secondPracBahrain);
+    const bahrainFp3Time = new ConvertLocalTime(thirdPracbahrain);
+    const bahrainQualiTimeLocal = new ConvertLocalTime(qualifyingBahrain);
+    const bahrainGpTimeLocal = new ConvertLocalTime(bahrainRaceLocal);
+    const saudiFp1Time = new ConvertLocalTime(firstPracLocal);
+    const saudiFp2Time = new ConvertLocalTime(secondPracLocal);
+    const saudiFp3Time = new ConvertLocalTime(thirdPracLocal);
+    const saudiQualiTimeLocal = new ConvertLocalTime(qualifyingLocal);
+    const saudiGpTimeLocal = new ConvertLocalTime(saudiRaceLocal);
 
-    const bahrainFp1Date = new FP1DateBahrain(firstPracLocal);
-    const bahrainFp2Date = new FP1DateBahrain(secondPracLocal);
-    const bahrainFp3Date = new FP1DateBahrain(thirdPracLocal);
-    // const bahrainQualiDateLocal = new FP1DateBahrain(bahrainQualiLocal);
-    // const bahrainGpDateLocal = new FP1DateBahrain(bahrainRaceLocal);
+    const bahrainFp1Date = new ConvertLocalDate(firstPracBahrain);
+    const bahrainFp2Date = new ConvertLocalDate(secondPracBahrain);
+    const bahrainFp3Date = new ConvertLocalDate(thirdPracbahrain);
+    const bahrainQualiDateLocal = new ConvertLocalDate(qualifyingBahrain);
+    const bahrainGpDateLocal = new ConvertLocalDate(bahrainRaceLocal);
+    const saudiFp1Date = new ConvertLocalDate(firstPracLocal);
+    const saudiFp2Date = new ConvertLocalDate(secondPracLocal);
+    const saudiFp3Date = new ConvertLocalDate(thirdPracLocal);
+    const saudiQualiDateLocal = new ConvertLocalDate(qualifyingLocal);
+    const saudiGpDateLocal = new ConvertLocalDate(saudiRaceLocal);
 
-    const markup = `<table>
+    const bahrainData = `<table>
     <tbody>
-      <tr>
       <tr>
       <td>Free Practice 1</td>
       <td>${bahrainFp1Date.getFormattedDate()}</td>
@@ -164,38 +196,42 @@ fetch("https://ergast.com/api/f1/current.json")
     </tr>
     </tbody>
   </table>`;
-    document.querySelector("table").insertAdjacentHTML("beforeend", markup);
-    // const timeStr = str.replace(/[Z:]/g, "");
-    // console.log(timeStr);
-    // // const time = Number(timeStr);
-    // // console.log(time);
-    // const newNum = Number(timeStr.toString().slice(0, -2));
-    // console.log(newNum);
-    // let timeEst = newNum - 500;
-    // console.log(timeEst);
-    // const timeEstB = timeEst.toString();
-    // console.log(timeEstB);
+    document
+      .getElementById("bahrain")
+      .insertAdjacentHTML("beforeend", bahrainData);
 
-    // Turn everything above in to a function to replace the times to EST for all races etc.
-    /////////////////////////////////
-    // const est = time - 5;
-    // console.log(est);
-
-    // let date = new Date(data.MRData.RaceTable.Races[0].FirstPractice.time);
-    // console.log(date);
-    // let timezoneOffset = date.getTimezoneOffset();
-    // console.log(timezoneOffset);
-
-    // // const utcDate = new Date(time);
-    // // console.log("UTC Date:", utcDate);
-
-    // const offsetMinutes = utcDate.getTimezoneOffset();
-    // console.log("Time Zone Offset (minutes):", offsetMinutes);
-    // // Step 3:
-    // const localTime = new Date(utcDate.getTime() - offsetMinutes * 60 * 1000);
-    // console.log("Local Time:", localTime);
-  });
-// .catch(error => console.log(error));
+    const saudiData = `<table>
+    <tbody>
+      <tr>
+      <td>Free Practice 1</td>
+      <td>${saudiFp1Date.getFormattedDate()}</td>
+      <td>${saudiFp1Time.getLocalDate()}</td>
+    </tr>
+    <tr>
+      <td>Free Practice 2</td>
+      <td>${saudiFp2Date.getFormattedDate()}</td>
+      <td>${saudiFp2Time.getLocalDate()}</td>
+    </tr>
+    <tr>
+      <td>Free Practice 3</td>
+      <td>${saudiFp3Date.getFormattedDate()}</td>
+      <td>${saudiFp3Time.getLocalDate()}</td>
+    </tr>
+    <tr>
+      <td>Qualifying</td>
+      <td>${saudiQualiDateLocal.getFormattedDate()}</td>
+      <td>${saudiQualiTimeLocal.getLocalDate()}</td>
+    </tr>
+    <tr>
+      <td>Grand Prix</td>
+      <td>${saudiGpDateLocal.getFormattedDate()}</td>
+      <td>${saudiGpTimeLocal.getLocalDate()}</td>
+    </tr>
+    </tbody>
+  </table>`;
+    document.getElementById("saudi").insertAdjacentHTML("beforeend", saudiData);
+  })
+  .catch(error => console.log(error));
 
 let coll = document.getElementsByClassName("collapsible");
 let i;
@@ -211,62 +247,3 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
-// var coll = document.getElementsByClassName("collapsiblealt");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.maxHeight) {
-//       content.style.maxHeight = null;
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//     }
-//   });
-// }
-
-// fetch("https://api.openf1.org/v1/drivers?driver_number=44&session_key=latest");
-// .then((response) => response.json())
-// .then((data) => console.log(data[0].team_name))
-// .catch((error) => console.error(error));
-// fetchData();
-// async function fetchData() {
-//   try {
-//     const driverName = document
-//       .getElementById("driverName")
-//       .value.toLowerCase();
-//     const response = await fetch(
-//       `https://api.openf1.org/v1/drivers?&session_key=latest${driverName}`
-//     );
-
-//     if (!response.ok) {
-//       throw new Error("Could not fetch resource");
-//     }
-
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// const utcDateString = "2024-02-29	11:30:00Z"; // UTC date string (example)
-// const utcDateWithoutMillis = utcDateString.slice(0, -5) + "Z";
-// const utcDate = new Date(utcDateWithoutMillis);
-// console.log("UTC Date:", utcDate.toISOString());
-
-// // Step 2:
-// const offsetMinutes = utcDate.getTimezoneOffset();
-// console.log("Time Zone Offset (minutes):", offsetMinutes);
-
-// // Step 3:
-// const localTime = new Date(utcDate.getTime() - offsetMinutes * 60 * 1000);
-// console.log("Local Time:", localTime.toISOString());
-
-// // Display Local Time
-// const localTimeString = localTime.toLocaleString();
-// document.getElementById(
-//   "localTime"
-// ).textContent = `Local Time: ${localTimeString}`;
